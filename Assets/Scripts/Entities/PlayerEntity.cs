b07a94bb706d75eb08703c6b1af62bf30f53d6c4
Sgánetz Bence <voidxb@gmail.com>
2017-02-17 03:04:50 +0100

    Whitespace corrections

--- Assets/Scripts/Entities/PlayerEntity.cs
+++ Assets/Scripts/Entities/PlayerEntity.cs
@@ -14,7 +14,7 @@ public class PlayerEntity : Singleton<PlayerEntity> {
     [Tooltip("Experience drop.")]
     public GameObject XPPickupObject;
     public AudioClip AudioPhoton, AudioScatter, AudioBeam;
-	public GameObject ProjectileEntity, BeamEntity, PlayerBody, DeathEffect;
+    public GameObject ProjectileEntity, BeamEntity, PlayerBody, DeathEffect;
     [Header("Settings")]
     [Tooltip("Don't drag the player entity as fast as the user moves their hand.")]
     public bool LeapSpeedLimit = false;
@@ -23,17 +23,17 @@ public class PlayerEntity : Singleton<PlayerEntity> {
     [Header("References")]
     public GameObject GameOverScreen;
 
-	AudioSource3D Source;
+    AudioSource3D Source;
     WeaponBase Weapon;
 
-	float Health = 100,
-	MovePos = -35,
-	SidePos = 0,
-	SinceSpawn = 3;
+    float Health = 100,
+    MovePos = -35,
+    SidePos = 0,
+    SinceSpawn = 3;
 
-	int Lives = 3, Score = -50;
+    int Lives = 3, Score = -50;
 
-	Texture2D GUIColor, GUITransparency;
+    Texture2D GUIColor, GUITransparency;
 
     public void AwardScore(int Score) {
         this.Score += Score;
@@ -44,12 +44,12 @@ public class PlayerEntity : Singleton<PlayerEntity> {
         Score += 25;
     }
 
-	public void PlaySound(AudioClip Sound, float Volume = 1) {
-		Source.PlayOneShot(Sound, Volume);
-	}
+    public void PlaySound(AudioClip Sound, float Volume = 1) {
+        Source.PlayOneShot(Sound, Volume);
+    }
 
-	public void WeaponPickup(WeaponKinds Kind) {
-		if (Weapon.Kind != Kind) {
+    public void WeaponPickup(WeaponKinds Kind) {
+        if (Weapon.Kind != Kind) {
             // Set new weapon
             int Level = Math.Max(1, Weapon.Level - 1);
             Destroy(Weapon);
@@ -65,61 +65,61 @@ public class PlayerEntity : Singleton<PlayerEntity> {
             GUITransparency = new Texture2D(1, 1);
             GUITransparency.SetPixel(0, 0, new Color(Weapon.DisplayColor.r, Weapon.DisplayColor.g, Weapon.DisplayColor.b, .5f));
             GUITransparency.Apply();
-		} else
+        } else
             Weapon.AddLevel();
-		Score += 50;
-	}
+        Score += 50;
+    }
 
-	void OnTriggerEnter(Collider col) {
+    void OnTriggerEnter(Collider col) {
         PickupBase Pickup = col.gameObject.GetComponent<PickupBase>();
         if (Pickup)
             Pickup.PickedUp();
         Projectile proj = col.gameObject.GetComponentInParent<Projectile>();
         if (proj && SinceSpawn >= 3) {
-			Health -= proj.Damage - Convert.ToInt32(proj.WeaponKind == Weapon.Kind);
-			if (Health <= 0) {
-				Health += 100;
-				Lives--;
-				if (Lives == 0) {
-					Instantiate(DeathEffect, transform.position, transform.rotation);
-					PlayerBody.SetActive(false);
+            Health -= proj.Damage - Convert.ToInt32(proj.WeaponKind == Weapon.Kind);
+            if (Health <= 0) {
+                Health += 100;
+                Lives--;
+                if (Lives == 0) {
+                    Instantiate(DeathEffect, transform.position, transform.rotation);
+                    PlayerBody.SetActive(false);
                     Weapon.Firing = false;
-				}
-				SinceSpawn = 0;
-			}
+                }
+                SinceSpawn = 0;
+            }
             Destroy(col.gameObject);
-		}
-	}
+        }
+    }
 
-	void Awake() {
+    void Awake() {
         Weapon = WeaponBase.AttachWeapon(WeaponKinds.Unassigned, gameObject);
-		WeaponPickup(WeaponKinds.Photon);
-		Source = GetComponent<AudioSource3D>();
+        WeaponPickup(WeaponKinds.Photon);
+        Source = GetComponent<AudioSource3D>();
         Settings.LeapSetupXZ();
     }
 
-	void OnGUI() {
-		if (Lives == 0) // Game over
+    void OnGUI() {
+        if (Lives == 0) // Game over
             return;
-		// Weapon and health bars
-		GUI.skin.label.fontStyle = FontStyle.BoldAndItalic;
-		GUI.skin.label.fontSize = Screen.height / 32;
-		int Margin = Screen.height / 20, BarLeft = Screen.width - 8 * Margin, BarTop = Screen.height - 2 * Margin, BarWidth = 7 * Margin,
-		Thickness = Screen.height / 200, MpT = Margin + Thickness;
-		Utilities.GUIRectangle(Margin, BarTop, BarWidth, Margin, Thickness, GUIColor);
-		Utilities.GUIRectangle(BarLeft, BarTop, BarWidth, Margin, Thickness, GUIColor);
-		BarWidth -= 2 * Thickness;
-		GUI.DrawTexture(new Rect(MpT, BarTop, Weapon.Level == 5 ? BarWidth : (BarWidth * Weapon.XP / 25), Margin), GUITransparency);
-		GUI.skin.label.alignment = TextAnchor.MiddleLeft;
-		GUI.Label(new Rect(MpT + 10, BarTop, BarWidth, Margin), "LEVEL " + Weapon.Level + " " + Weapon.DisplayName);
-		GUI.DrawTexture(new Rect(BarLeft + Thickness, BarTop, BarWidth * Health / 100, Margin), GUITransparency);
-		GUI.skin.label.alignment = TextAnchor.MiddleRight;
-		GUI.Label(new Rect(BarLeft - 10, BarTop, BarWidth, Margin), "LIVES: " + Lives);
-		// Score
-		GUI.skin.label.fontSize *= 2;
-		GUI.skin.label.alignment = TextAnchor.LowerCenter;
-		GUI.Label(new Rect(0, 0, Screen.width, BarTop + Margin), Score.ToString());
-	}
+        // Weapon and health bars
+        GUI.skin.label.fontStyle = FontStyle.BoldAndItalic;
+        GUI.skin.label.fontSize = Screen.height / 32;
+        int Margin = Screen.height / 20, BarLeft = Screen.width - 8 * Margin, BarTop = Screen.height - 2 * Margin, BarWidth = 7 * Margin,
+        Thickness = Screen.height / 200, MpT = Margin + Thickness;
+        Utilities.GUIRectangle(Margin, BarTop, BarWidth, Margin, Thickness, GUIColor);
+        Utilities.GUIRectangle(BarLeft, BarTop, BarWidth, Margin, Thickness, GUIColor);
+        BarWidth -= 2 * Thickness;
+        GUI.DrawTexture(new Rect(MpT, BarTop, Weapon.Level == 5 ? BarWidth : (BarWidth * Weapon.XP / 25), Margin), GUITransparency);
+        GUI.skin.label.alignment = TextAnchor.MiddleLeft;
+        GUI.Label(new Rect(MpT + 10, BarTop, BarWidth, Margin), "LEVEL " + Weapon.Level + " " + Weapon.DisplayName);
+        GUI.DrawTexture(new Rect(BarLeft + Thickness, BarTop, BarWidth * Health / 100, Margin), GUITransparency);
+        GUI.skin.label.alignment = TextAnchor.MiddleRight;
+        GUI.Label(new Rect(BarLeft - 10, BarTop, BarWidth, Margin), "LIVES: " + Lives);
+        // Score
+        GUI.skin.label.fontSize *= 2;
+        GUI.skin.label.alignment = TextAnchor.LowerCenter;
+        GUI.Label(new Rect(0, 0, Screen.width, BarTop + Margin), Score.ToString());
+    }
 
     void Update() {
         if (Lives == 0) {
@@ -129,12 +129,12 @@ public class PlayerEntity : Singleton<PlayerEntity> {
         }
         Weapon.Firing = AutoFire; // Firing to default
         Health = Mathf.Min(Health + Time.deltaTime * 3f, 100); // Refill health
-		// Flashing after death
-		SinceSpawn += Time.deltaTime;
-		bool ShowBody = SinceSpawn < 3 ? Mathf.FloorToInt(SinceSpawn * 4) % 2 == 0 : true;
-		if (ShowBody != PlayerBody.activeSelf)
-			PlayerBody.SetActive(ShowBody);
-		// Movement
+                                                               // Flashing after death
+        SinceSpawn += Time.deltaTime;
+        bool ShowBody = SinceSpawn < 3 ? Mathf.FloorToInt(SinceSpawn * 4) % 2 == 0 : true;
+        if (ShowBody != PlayerBody.activeSelf)
+            PlayerBody.SetActive(ShowBody);
+        // Movement
         Vector2 LeapTarget = LeapMotion.Instance.PalmOnScreenXZ();
         float VerticalMovement = 0, HorizontalMovement = 0;
         if (LeapTarget.x != -1 && LeapTarget.y != -1) {
@@ -161,5 +161,5 @@ public class PlayerEntity : Singleton<PlayerEntity> {
         SidePos = Mathf.Clamp(SidePos + HorizontalMovement * Time.deltaTime * 100f, -75, 75);
         transform.position = new Vector3(SidePos, 25, MovePos + MapHandler.Instance.MapPos);
         transform.rotation = Quaternion.Euler(0, 0, -HorizontalMovement * 15f);
-	}
+    }
 }
