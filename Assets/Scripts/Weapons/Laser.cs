d34febd1da257924db71b9385976cb9f6f9082c2
Sgánetz Bence <voidxb@gmail.com>
2017-04-28 02:17:40 +0200

    Constantly firing laser

--- Assets/Scripts/Weapons/Laser.cs
+++ Assets/Scripts/Weapons/Laser.cs
@@ -8,16 +8,26 @@ namespace Weapons {
     /// </summary>
     [AddComponentMenu("Weapons / Laser")]
     public class Laser : WeaponBase {
+        /// <summary>
+        /// As a beam must be displayed each frame, a different cooldown must be used here.
+        /// </summary>
+        float ActualCooldown = 0;
+
         protected override void Setup() {
             _Kind = WeaponKinds.Laser;
             _DisplayName = "LASER";
-            Cooldown = .0625f;
+            Cooldown = 0;
         }
 
         protected override void Shoot() {
             Projectile projectile = Instantiate(PlayerEntity.Instance.BeamEntity, transform.position + new Vector3(0, 0, 12), transform.rotation).GetComponent<Projectile>();
             projectile.Player = true;
-            projectile.Damage = Level + 1;
+            ActualCooldown -= Time.deltaTime;
+            if (ActualCooldown <= 0) {
+                ActualCooldown += .0625f; // The beam actually hits 16 times a second
+                projectile.Damage = Level + 1;
+            } else
+                projectile.Damage = 0;
             projectile.WeaponKind = WeaponKinds.Laser;
             projectile.transform.localScale = new Vector3(Level, 1, 1);
             PlayerEntity.Instance.PlaySound(PlayerEntity.Instance.AudioBeam, .5f);
