7d8b4a88273f33d9dfaae9b5df6d31e57caf9ef0
VoidX <voidxb@gmail.com>
2017-09-12 13:23:41 +0200

    Redesigned menu

---
+++ Assets/Scripts/Helpers/SimpleLookAt.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Helpers {
+    /// <summary>
+    /// Makes an object look at another.
+    /// </summary>
+    [AddComponentMenu("Helpers / Simple Look At")]
+    public class SimpleLookAt : MonoBehaviour {
+        [Tooltip("The object to look at.")]
+        public Transform Target;
+
+        void Update() {
+            transform.LookAt(Target.position);
+        }
+    }
+}
