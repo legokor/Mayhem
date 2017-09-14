40db2748d074b07cc3ea26a915d682339ae654ab
VoidX <voidxb@gmail.com>
2017-09-14 02:00:11 +0200

    Space Court. For people in space. Judge space sun presiding. Bam. Guilty. Of being in space. I'm in space.

---
+++ Assets/Scripts/Helpers/ObjectRotator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Helpers {
+    /// <summary>
+    /// Rotates an object.
+    /// </summary>
+    [AddComponentMenu("Helpers / Object Rotator")]
+    public class ObjectRotator : MonoBehaviour {
+        /// <summary>
+        /// Rotation speed multiplier.
+        /// </summary>
+        public float Speed = 1;
+
+        [Header("Axes")]
+        public bool X, Y = true, Z;
+
+        /// <summary>
+        /// Rotating.
+        /// </summary>
+        void Update() {
+            Vector3 Angles = transform.eulerAngles;
+            float Addition = Time.deltaTime * Speed;
+            if (X)
+                Angles.x += Addition;
+            if (Y)
+                Angles.y += Addition;
+            if (Z)
+                Angles.z += Addition;
+            transform.eulerAngles = Angles;
+        }
+    }
+}
