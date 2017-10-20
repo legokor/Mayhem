96b845eff194f70120dcae26c20d308a6ec17a5d
VoidX <voidxb@gmail.com>
2017-10-20 13:33:53 +0200

    Pause menu

---
+++ Assets/Scripts/Helpers/KeyHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Helpers {
+    /// <summary>
+    /// Do something when a key is pressed.
+    /// </summary>
+    [AddComponentMenu("Helpers / Key Handler")]
+    class KeyHandler : MonoBehaviour {
+        public KeyCode Key = KeyCode.None;
+        public UnityEvent Event;
+
+        void Update() {
+            if (Input.GetKeyDown(Key))
+                Event?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Menus {
+    /// <summary>
+    /// Pause menu behaviour.
+    /// </summary>
+    [AddComponentMenu("Menus / Pause")]
+    public class PauseMenu : MonoBehaviour {
+        /// <summary>
+        /// Cached time scale in case it's modified.
+        /// </summary>
+        float OldTimeScale = 1;
+
+        /// <summary>
+        /// Toggle pause menu.
+        /// </summary>
+        public void Toggle() {
+            gameObject.SetActive(!gameObject.activeSelf);
+        }
+
+        /// <summary>
+        /// Continue playing.
+        /// </summary>
+        public void Resume() {
+            gameObject.SetActive(false);
+        }
+
+        /// <summary>
+        /// Freeze time when this menu is shown.
+        /// </summary>
+        void OnEnable() {
+            OldTimeScale = Time.timeScale;
+            Time.timeScale = 0;
+        }
+
+        /// <summary>
+        /// Unfreeze time when this menu is hidden.
+        /// </summary>
+        void OnDisable() {
+            Time.timeScale = OldTimeScale;
+        }
+    }
+}
